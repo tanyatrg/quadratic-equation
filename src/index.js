d0f6e4cbb5e78d8199718154f9c51618bc78a47e
tanyatrg <36745237+tanyatrg@users.noreply.github.com>
2018-02-28 19:34:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-  var c2,x1,x2,d,a,b,c;
-  //x^2 \-?
-  a = Number(equation.match(/\-?\d+/));
-  c2 = equation.match(/(\- )?\d+/g);
-  b = (c2[2]);
-  c = (c2[3]);
-  b = Number(b.replace(/\s/g, ""));
-  c = Number(c.replace(/\s/g, ""));
-  d = b * b - 4 * a * c;
-  x1 = -b / ( 2 * a ) - Math.sqrt( d ) / ( 2 * a );
-  x2 = -b / ( 2 * a ) + Math.sqrt( d ) / ( 2 * a );
-  x1=Math.round(x1);
-  x2=Math.round(x2);
-  //var mas = new Array(x1,x2) ;
-  function compareNumbers (x1, x2) {
-    return x1 - x2;
-  }
-
-  var mas = new Array(x1,x2) ;
-
-  mas.sort(compareNumbers);
-
-return mas;
 }
